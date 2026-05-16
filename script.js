464b04da34f306f48bf88c2fad14be64df3bc0bd
NadiQueen1 <nadianorgah24@gmail.com>
2026-05-16 16:49:14 +0000

    website upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById("startBtn");
+const contactBtn = document.getElementById("contactBtn");
+
+startBtn.addEventListener("click", function () {
+  alert("Let's start building your first website 🚀");
+});
+
+contactBtn.addEventListener("click", function () {
+  alert("Contact feature coming soon!");
+});
